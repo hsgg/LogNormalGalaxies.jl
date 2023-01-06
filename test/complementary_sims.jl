ed56380a9fe197b68541b41d279bb46cb09edd28
Henry Gebhardt <gebhardt@caltech.edu>
2023-01-05 23:10:49 -0800

    test/complementary_simulations.jl: nicer test

--- test/complementary_sims.jl
+++ test/complementary_sims.jl
@@ -5,74 +5,107 @@ using MeasurePowerSpectra
 using DelimitedFiles
 using Statistics
 using PyPlot
+using Random
 using Test
 
 @testset "Complimentary simulations" begin
-    nbar = 1e-4
-    L = 1e3
-    nmesh = 64
-    bias = 1.5
+    include("lib.jl")
+
+    nbar = 1e-3
+    L = 0.5e3
+    n_sim = 64
+    n_est = 64
+    bias = 1.0
     f = 0.7
 
     LLL = L .* [1,1,1]
-    nnn = nmesh .* [1,1,1]
+    nnn_est = n_est .* [1,1,1]
     box_center = [0,0,0]
-    opts = (nbar=nbar, lmax=4, do_mu_leakage=true, subtract_shotnoise=true, voxel_window_power=1)
+    vox = 1
+    opts = (nbar=nbar, lmax=4, do_mu_leakage=true, subtract_shotnoise=true, voxel_window_power=vox)
+
+    id = "vox=$vox, nsim=$n_sim, nest=$n_est"
 
     #keq = 2e-2
     #c = 3 * keq^4
     #a = 2e4 * 4 * keq^3
     #pk(k) = a * k / (c + k^4)
-    D = 0.82
+    D = 0.4
     data = readdlm((@__DIR__)*"/matterpower.dat", comments=true)
     _pk = Spline1D(data[:,1], data[:,2], extrapolation=Splines.powerlaw)
     pk(k) = D^2 * _pk(k)
 
 
-
-
     Random.seed!(reinterpret(UInt64, time()))
     seed = rand(UInt64, 4)
+    #seed = UInt64[0x01ca4eddf1fdc165, 0xd87d80f08f1e36f8, 0x0ecc4300d7f0a61c, 0xeb0ab21155f56174]
     @show seed
     Random.seed!(seed)
 
-    @time (x⃗1, Ψ1), (x⃗2, Ψ2) = simulate_galaxies(nbar, L, pk; nmesh, bias, f=true, extra_phases=π)
-    #@time x⃗1, Ψ1 = simulate_galaxies(nbar, L, pk; nmesh, bias, f=true)
-    #@time x⃗2, Ψ2 = simulate_galaxies(nbar, L, pk; nmesh, bias, f=true)
+
+    @time (x⃗1, Ψ1), (x⃗2, Ψ2) = simulate_galaxies(nbar, L, pk; nmesh=n_sim, bias, f=true, extra_phases=π)
+    #@time x⃗1, Ψ1 = simulate_galaxies(nbar, L, pk; nmesh=n_sim, bias, f=true)
+    #@time x⃗2, Ψ2 = simulate_galaxies(nbar, L, pk; nmesh=n_sim, bias, f=true)
 
 
-    k1, pk1, nmodes1 = xgals_to_pkl_planeparallel(x⃗1, LLL, nnn, box_center; opts...)
-    k2, pk2, nmodes2 = xgals_to_pkl_planeparallel(x⃗2, LLL, nnn, box_center; opts...)
+    k1, pk1, nmodes1 = xgals_to_pkl_planeparallel(x⃗1, LLL, nnn_est, box_center; opts...)
+    k2, pk2, nmodes2 = xgals_to_pkl_planeparallel(x⃗2, LLL, nnn_est, box_center; opts...)
+    k1 = k1[2:end]
+    k2 = k2[2:end]
+    pk1 = pk1[2:end,:]
+    pk2 = pk2[2:end,:]
+
+    #figure()
+    #title("Real space")
+    #hlines(1/nbar, extrema(k1)..., color="0.75")
+    #plot(k1, bias^2 .* pk.(k1), "k-")
+    #plot(k1, pk1[:,:], "-")
+    #gca().set_prop_cycle(nothing)
+    #plot(k2, pk2[:,:], "--")
+    #plot(k1, middle.(pk1, pk2), "k:")
+    #xlim(0, 0.25)
 
     figure()
-    title("Real space")
-    plot(k1, bias^2 .* pk.(k1), "k-")
+    title("Real space: $id")
+    hlines(1, extrema(k1)..., color="0.75")
+    plot(k1, pk1[:,1] ./ (bias^2 .* pk.(k1)), "-")
+    plot(k2, pk2[:,1] ./ (bias^2 .* pk.(k1)), "-")
+    plot(k2, (pk1 .+ pk2)[:,1] ./ 2 ./ (bias^2 .* pk.(k1)), "k:")
+    xlim(0, 0.25)
+    ylim(0.6, 1.4)
+
+    #return
+
+    #### to redshift space
+
+    los = [0, 0, 1]
+    apply_RSD!(x⃗1, Ψ1, f, los)
+    apply_RSD!(x⃗2, Ψ2, f, los)
+    x⃗1 = MeasurePowerSpectra.periodic_boundaries(x⃗1, LLL, box_center)
+    x⃗2 = MeasurePowerSpectra.periodic_boundaries(x⃗2, LLL, box_center)
+    k1, pk1, nmodes1 = xgals_to_pkl_planeparallel(x⃗1, LLL, nnn_est, box_center; opts...)
+    k2, pk2, nmodes2 = xgals_to_pkl_planeparallel(x⃗2, LLL, nnn_est, box_center; opts...)
+
+    β = f / bias
+    pkm_kaiser = @. bias^2 * Arsd_Kaiser(β, (0:4)') * pk(k1)
+
+    figure()
+    title("Redshift space: $id")
+    hlines(1/nbar, extrema(k1)..., color="0.75")
+    plot(k1, pkm_kaiser[:,:], "-", lw=0.5)
+    gca().set_prop_cycle(nothing)
     plot(k1, pk1[:,:], "-")
     gca().set_prop_cycle(nothing)
     plot(k2, pk2[:,:], "--")
     plot(k1, middle.(pk1, pk2), "k:")
-    xlim(0, 0.15)
-
-
-    function add_RSD!(x⃗, Ψ, f, los)
-        Ngals = size(x⃗,2)
-        for i=1:Ngals
-            x⃗[:,i] .+= f * (Ψ[:,i]' * los) * los
-        end
-        return x⃗
-    end
-    los = [0, 0, 1]
-    add_RSD!(x⃗1, Ψ1, f, los)
-    add_RSD!(x⃗2, Ψ2, f, los)
-    x⃗1 = MeasurePowerSpectra.periodic_boundaries(x⃗1, LLL, box_center)
-    x⃗2 = MeasurePowerSpectra.periodic_boundaries(x⃗2, LLL, box_center)
-    k1, pk1, nmodes1 = xgals_to_pkl_planeparallel(x⃗1, LLL, nnn, box_center; opts...)
-    k2, pk2, nmodes2 = xgals_to_pkl_planeparallel(x⃗2, LLL, nnn, box_center; opts...)
-
+    xlim(0, 0.25)
 
     figure()
-    title("Redshift space")
-    plot(k1, pk1, "-")
-    gca().set_prop_cycle(nothing)
-    plot(k2, pk2, "--")
+    title("Redshift space: $id")
+    hlines(1, extrema(k1)..., color="0.75")
+    plot(k1, pk1[:,1] ./ pkm_kaiser[:,1], "-")
+    plot(k2, pk2[:,1] ./ pkm_kaiser[:,1], "-")
+    plot(k2, (pk1 .+ pk2)[:,1] ./ 2 ./ pkm_kaiser[:,1], "k:")
+    xlim(0, 0.25)
+    ylim(0.6, 1.4)
 end
